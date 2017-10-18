2e5b1620b84702162297464cb360748e82640886
ChouVue <30632978+ChouVue@users.noreply.github.com>
2017-10-18 10:40:19 -0500

    Video 105: IBOutlet Connection Connected

---
+++ DevSlopeDreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DevSlopeDreamLister
+//
+//  Created by Chou Vue on 10/18/17.
+//  Copyright © 2017 VUE. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell
+{
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    @IBOutlet weak var detailLbl: UILabel!
+    
+    
+}
